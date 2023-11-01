68047fff95c4bfddc34614b69b0f9fb4338a18b4
Shabyte <sharislavilla@gmail.com>
2023-11-01 20:25:51 +0800

    modified

--- dbConnection/mysqlconfig_connection.php
+++ dbConnection/mysqlconfig_connection.php
@@ -1,8 +1,8 @@
 <?php
     DEFINE('DB_USER', 'root');
-    DEFINE('DB_PASSWORD', 'root');
+    DEFINE('DB_PASSWORD', '');
     DEFINE('DB_HOST', 'localhost');
-    DEFINE('DB_NAME', 'db');
+    DEFINE('DB_NAME', 'dbschedule');
 
     $dbc = @mysqli_connect(DB_USER, DB_PASSWORD,DB_HOST, DB_NAME)
     OR die('Could not connect to MySQL: ' . mysqli_connect_error());
--- index.php
+++ index.php
@@ -1,18 +1,22 @@
 <?php
-include_once("dbConnection/mysqlconfig_connection.php");
-include_once("function/fetch.php")
+include_once("./dbConnection/mysqlconfig_connection.php");
+include_once("./function/fetch.php")
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Document</title>
+
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Sample CRUD</title>
+
 </head>
 <body>
     <h1>My Subjects</h1>
-    <a href="forms/addform.php">Add Subject</a><br/><br/>
+    <a href="./forms/addform.php">Add Subject</a><br/><br/>
     <table width='100%' border= 1>
-        <tr bgcolor= '#CCCCCC'>
+        <tr bgcolor= 'CCCCCC'>
             <td>ID</td>
             <td>Subject Code</td>
             <td>Subject Name</td>
@@ -24,7 +28,7 @@ include_once("function/fetch.php")
             echo "<tr>".$res['subject_id']."</td>";
             echo "<tr>".$res['subject_code']."</td>";
             echo "<tr>".$res['subject_name']."</td>";
-            echo "<tr><a href=\"forms/editform.php?id=$res[subject_id]\">Edit</a> |
+            echo "<tr><a href=\"./forms/editform.php?id=$res[subject_id]\">Edit</a> |
                     <a href=\"forms/delete.php?id=$res[subject_id]\"
                     Onclick=\"return confirm('Are you sure you want to delete?')\"> Delete</a></td>";
         }
